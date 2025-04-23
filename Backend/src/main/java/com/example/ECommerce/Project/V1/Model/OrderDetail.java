c071b453c47171d80927d8cdc8fa77e967094313
dungle2906 <vandung290603@gmail.com>
2025-04-23 22:23:09 +0700

    Fix address of order

--- Backend/src/main/java/com/example/ECommerce/Project/V1/Model/OrderDetail.java
+++ Backend/src/main/java/com/example/ECommerce/Project/V1/Model/OrderDetail.java
@@ -39,7 +39,8 @@ public class OrderDetail extends BaseEntity {
     @JoinColumn(name = "user_id")
     private User user;
 
-    private Integer addressId;
+    @OneToOne()
+    private Address addressId;
 
     @ManyToOne()
     @JoinColumn(name = "voucher_id")
