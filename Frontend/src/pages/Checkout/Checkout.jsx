9d8ec232d65d5e2ccea042c00fddb0230cc83b66
GiaBao <Hoduonggiabao1512@gmail.com>
2025-04-24 21:53:05 +0700

    fix(checkout): Fix checkout with vnpay

--- Frontend/src/pages/Checkout/Checkout.jsx
+++ Frontend/src/pages/Checkout/Checkout.jsx
@@ -6,17 +6,20 @@ import { getToken } from "../Login/app/static";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { createOrder } from "../../services/orderService";
+import { useCart } from "../../context/CartContext";
 
 function Checkout() {
   const [searchParams] = useSearchParams();
 
   const payment = searchParams.get("payment");
   const orderCreatedRef = useRef(false);
+  const { clearCart } = useCart();
 
   const handleCreateOrder = async (payload) => {
     try {
       const response = await createOrder(payload);
       toast.success("Đơn hàng đã được tạo thành công");
+      await clearCart();
       return response.data;
     } catch (error) {
       toast.error("Lỗi khi tạo đơn hàng");
